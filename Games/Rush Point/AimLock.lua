e6ebd6a5d985599ac0940b644172131023100234
ZynxXCandy <78780988+ZynxXCandy@users.noreply.github.com>
2022-01-08 14:36:29 -0600

    The Potato shit

---
+++ Games/Rush Point/AimLock.lua
@@ -0,0 +1,5 @@
+-- // Dependencies
+local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZynxXCandy/rush-point/master/Universal/Aiming/Load.lua"))()("Module", "RushPoint")
+
+-- // Start
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZynxXCandy/rush-point/master/Universal/Aiming/Settings/AimLock.lua"))()
